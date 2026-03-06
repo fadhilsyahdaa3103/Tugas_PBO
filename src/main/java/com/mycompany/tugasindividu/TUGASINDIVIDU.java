a29d57cb2a54323a575bb420646623e841e8024e
VICTUS <VICTUS@LAPTOP-61AICB1G>
2026-03-06 16:19:11 +0700

    first commit

---
+++ src/main/java/com/mycompany/tugasindividu/TUGASINDIVIDU.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugasindividu;
+
+/**
+ *
+ * @author VICTUS
+ */
+public class TUGASINDIVIDU {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
